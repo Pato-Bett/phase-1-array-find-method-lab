ae039c5178ed819fffd506773f9d08f203f57fd6
Pato-Bett <patobett588@gmail.com>
2023-10-19 14:26:38 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+let superbowlWin = (record) =>{
+    let yearWon = record.find(record=>record.result==="W")
+    if (yearWon) {
+        return yearWon.year
+    }else{
+        return undefined
+    }
+}
